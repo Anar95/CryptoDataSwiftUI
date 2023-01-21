772a8c1a86c427eb96521044663ef1331e974c29
Anar95 <anarabbas458@gmail.com>
2023-01-22 01:57:02 +0300

    Color Creat

---
+++ CryptoData/CryptoData/CryptoDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoDataApp.swift
+//  CryptoData
+//
+//  Created by Anar Abbas on 22.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CryptoData/CryptoData/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  CryptoData
+//
+//  Created by Anar Abbas on 22.01.2023.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let theme = ColorTheme()
+    
+}
+struct ColorTheme{
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
