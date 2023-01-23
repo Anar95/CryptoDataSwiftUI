dbb173671e96aaea78c853fc947fcb509c5620b4
Anar95 <anarabbas458@gmail.com>
2023-01-24 00:38:12 +0300

    Creat PreviewProvider

---
+++ CryptoData/CryptoData/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,20 @@
+//
+//  CoinRowView.swift
+//  CryptoData
+//
+//  Created by Anar Abbas on 23.01.2023.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text("Hello")
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView()
+    }
+}
