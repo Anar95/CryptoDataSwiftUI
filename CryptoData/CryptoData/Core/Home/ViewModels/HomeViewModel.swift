50af3f70bfccb2c9293872ee375526f1446d80e6
Anar95 <anarabbas458@gmail.com>
2023-02-06 00:04:42 +0300

    Update HomeView & Portfilio

--- CryptoData/CryptoData/Core/Home/ViewModels/HomeViewModel.swift
+++ CryptoData/CryptoData/Core/Home/ViewModels/HomeViewModel.swift
@@ -14,7 +14,7 @@ class HomeViewModel: ObservableObject{
     init(){
         DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
             self.allCoins.append(DeveloperPreview.instance.coin)
-            self.allCoins.append(DeveloperPreview.instance.coin)
+            self.portfolioCoins.append(DeveloperPreview.instance.coin)
         }
     }
     
