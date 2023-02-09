8d0a8c2e3f75b08063ca4c9d7bb7d5e8e480c6dd
Anar95 <anarabbas458@gmail.com>
2023-02-09 21:50:55 +0300

    ProgressView creat

--- CryptoData/CryptoData/Core/CoinImage/CoinImageView.swift
+++ CryptoData/CryptoData/Core/CoinImage/CoinImageView.swift
@@ -6,16 +6,46 @@
 //
 
 import SwiftUI
-  
-
-struct CoinImageView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class CoinImageViewModel: ObservableObject{
+    
+    @Published var image: UIImage? = nil
+    @Published var isLoading: Bool = false
+    
+    
+    
+    init() {
+        getImage()
+    }
+    private func getImage() {
+        
     }
 }
+
+struct CoinImageView: View {
+    
+    @StateObject var vm: CoinImageViewModel = CoinImageViewModel()
+    
+    var body: some View {
+        ZStack{
+            if let image = vm.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }else if vm.isLoading{
+                ProgressView()
+            }else {
+                Image(systemName: "qusetionmarl")
+                    .foregroundColor(Color.theme.secondaryText)
+            }
+        }
+    }
+}
+
 
 struct CoinImageView_Previews: PreviewProvider {
     static var previews: some View {
         CoinImageView()
+            .padding()
+            .previewLayout(.sizeThatFits)
     }
 }
