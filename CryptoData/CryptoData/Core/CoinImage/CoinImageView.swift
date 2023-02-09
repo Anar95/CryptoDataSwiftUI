e42f16964f49dc0bcf752308c8532d03ebdcec4f
Anar95 <anarabbas458@gmail.com>
2023-02-09 21:38:37 +0300

    Coin Image View Creat

---
+++ CryptoData/CryptoData/Core/CoinImage/CoinImageView.swift
@@ -0,0 +1,21 @@
+//
+//  CoinImageView.swift
+//  CryptoData
+//
+//  Created by Anar Abbas on 9.02.2023.
+//
+
+import SwiftUI
+  
+
+struct CoinImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CoinImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinImageView()
+    }
+}
